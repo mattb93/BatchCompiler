4311b657b3486479776c4517353c7f5eddf3d5ee
Eric Williamson <ericrw96@vt.edu>
2015-11-17 22:03:25 -0500

    add in the ability to compile and run the student files

--- src/compiler/Driver.java
+++ src/compiler/Driver.java
@@ -4,16 +4,33 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 
 //import javax.tools.JavaCompiler;
 //import javax.tools.ToolProvider;
 
 import java.util.Enumeration;
+import java.util.List;
 //import java.util.Scanner;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
 
 public class Driver
 {
@@ -25,7 +42,7 @@ public class Driver
     private static String packageName;
     private static ArrayList<File> libraries;
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         if (args.length == 0)
         {
@@ -78,11 +95,20 @@ public class Driver
             System.out.println("==============================");
             extractFiles(args[0]);
             System.out.println("==============================");
-            processFiles();
+            try
+            {
+                processFiles();
+            }
+            catch (IOException e)
+            {
+
+                e.printStackTrace();
+                System.out.println("Could not access a class path file");
+            }
         }
     }
 
-    private static void setUpEnvironment(String[] args)
+    private static void setUpEnvironment(String[] args) throws IOException
     {
         System.out.println("CS 2114 batch compiler\n");
 
@@ -95,12 +121,13 @@ public class Driver
         System.out.println("Using package name: " + args[2]);
 
         String inputLibraries = "Using Libraries: ";
+        libraries = new ArrayList<File>();
         for (int i = 3; i < args.length; i++)
         {
             inputLibraries += args[i];
 
             libraries.add(new File(args[i]));
-
+            ClassPathHacker.addFile(new File(args[i]));
             if (i < args.length - 1)
             {
                 inputLibraries += ", ";
@@ -133,7 +160,7 @@ public class Driver
                 .println("Succesfully extracted " + zipName + " into " + dest);
     }
 
-    private static void processFiles()
+    private static void processFiles() throws IOException
     {
         File rootFolder = new File(root);
         System.out.println("Navigating to: " + rootFolder.getPath());
@@ -154,7 +181,7 @@ public class Driver
         }
     }
 
-    private static void processProject(File individualRoot)
+    private static void processProject(File individualRoot) throws IOException
     {
         String pid = individualRoot.getName();
         System.out.println("===== Processing: " + pid);
@@ -192,7 +219,32 @@ public class Driver
                     + File.separator + packageName);
 
             System.out.println("Attempting to compile " + pid);
-            compile(extractedSubmission);
+
+            /*
+             * This is where we manually copy in the QueueReader so we can
+             * eventually run their submission
+             */
+            try
+            {
+
+                Files.copy(new File("QueueReader.java").toPath(),
+                        new File(extractedSubmissionPath + File.separator
+                                + projectName + File.separator + "src"
+                                + File.separator + packageName + File.separator
+                                + "QueueReader.java").toPath(),
+                        StandardCopyOption.COPY_ATTRIBUTES);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                System.out.println(
+                        "We did not have a correctly setup project :(");
+            }
+            if (compile(extractedSubmission))
+            {
+                runProgram(new File(extractedSubmissionPath + File.separator
+                        + projectName + File.separator + "src"));
+            }
         }
     }
 
@@ -238,12 +290,143 @@ public class Driver
         return destDir;
     }
 
-    private static void compile(File extractedSubmission)
+    private static boolean compile(File extractedSubmission) throws IOException
     {
-        System.out.println("Compiling the files is not supported yet");
-        // JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        // String compilerArgs = "";
 
-        // compiler.run(null, null, null, compilerArgs);
+        /*
+         * Get all of the java files in the extracted submissions directory
+         */
+        List<File> javaFiles = Collections.emptyList();
+        try
+        {
+            javaFiles = Arrays
+                    .asList(extractedSubmission.listFiles((dir, name) ->
+                    {
+                        // llambda expression so we only get .java files
+                        return name.contains(".java");
+                    }));
+        }
+        catch (NullPointerException e)
+        {
+            System.out.println("Found no Files");
+            return false;
+        }
+
+        /**
+         * Initialize diagnostics collector to report compile errors
+         */
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+
+        /*
+         * We need the jdk to get the compiler
+         */
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler
+                .getStandardFileManager(diagnostics, null, null);
+
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager
+                .getJavaFileObjectsFromFiles(javaFiles);
+
+        /**
+         * Make sure the file manager gets the command line arguments
+         * (libraries) as well
+         */
+        fileManager.setLocation(StandardLocation.CLASS_PATH, libraries);
+
+        List<String> optionList = new ArrayList<String>();
+        // set compiler's classpath to be same as the runtime's
+        optionList.addAll(Arrays.asList("-classpath",
+                System.getProperty("java.class.path")));
+
+        // any other options you want would be added here
+
+        // prepare to run the compiler with these arguments
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager,
+                diagnostics, optionList, null, compilationUnits);
+
+        // run the java compiler and check the success of it
+        boolean success = task.call();
+
+        if (!success)
+        {
+            System.out.println(
+                    "Failed to Compile, would reccomend looking manualy");
+            for (Diagnostic<? extends JavaFileObject> d : diagnostics
+                    .getDiagnostics())
+            {
+                System.out.println(d.toString());
+            }
+            return false;
+        }
+        else
+        {
+            System.out.println("Successfully compiled the program");
+            return true;
+        }
+    }
+
+    private static void runProgram(File extractedSubmission)
+    {
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                String[] params = new String[0];
+                URLClassLoader loader = null;
+                try
+                {
+                    loader = new URLClassLoader(
+                            new URL[] { extractedSubmission.toURI().toURL() },
+                            ClassLoader.getSystemClassLoader());
+                    loader.loadClass("rollercoaster.QueueReader")
+                            .getDeclaredMethod("main",
+                                    new Class[] { String[].class })
+                            .invoke(null, new Object[]
+                    { (Object[]) params });
+                    loader.close();
+                }
+                catch (MalformedURLException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (IllegalAccessException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (IllegalArgumentException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (InvocationTargetException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (NoSuchMethodException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (SecurityException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (ClassNotFoundException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                catch (IOException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
